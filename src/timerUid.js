9352dd03b9320b60af3512879484502526ecffd3
Wei <wrenth04@gmail.com>
2016-07-25 15:02:39 +0800

    add encode & decode

--- src/timerUid.js
+++ src/timerUid.js
@@ -4,6 +4,8 @@ var lastTimestamp = 0;
 var lastRandom = new Array(12);
 
 var timerUid = {
+  encode: encode,
+  decode: decode,
   tuid: function() {
     var now = Date.now();
     var time = [];
@@ -35,6 +37,28 @@ var timerUid = {
     }, '');
   }
 };
+
+function encode(timestamp) {
+  var time = [];
+  for(var i = 0 ; i < 8; i++) {
+    time.unshift(timestamp % CHARS.length);
+    timestamp = Math.floor(timestamp / CHARS.length);
+  }
+
+  return time.concat(lastRandom).reduce(function(a, b) {
+    return a + CHARS.charAt(b);
+  }, '');
+}
+
+function decode(tuid) {
+  if(!tuid || tuid.length < 8) return -1;
+  var timestamp = 0;
+  var t = tuid.split('');
+  for(var i = 0 ; i < 8 ; i++) {
+    timestamp = timestamp*CHARS.length + CHARS.indexOf(t[i]);
+  }
+  return timestamp;
+}
 
 module.exports = timerUid;
 
